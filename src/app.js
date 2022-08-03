f89f4cfd426c5df70c7e72f3ec7bbc73dcfda5db
Klaudia Stopczynska <k.stopczynska@gmail.com>
2022-08-03 10:51:50 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const hamburger = document.querySelector('.hamburger-container');
+const menu = document.querySelector('.menu');
+const arrows = document.querySelectorAll('.arrow');
+const overlay = document.querySelector('.overlay');
+const buttonWrapper = document.querySelector('.nav-button-wrapper');
+const featuresList = document.querySelector('#features-list');
+const companyList = document.querySelector('#company-list');
+
+const menuHandler = () => {
+menu.classList.toggle('active');
+hamburger.classList.toggle('active');
+overlay.classList.toggle('active');
+}
+
+const submenuHandler = (e) => {
+    e.target.nextElementSibling.classList.toggle('active');
+    const dropdown = e.target.nextElementSibling.parentNode.nextElementSibling;
+    if (e.target.nextElementSibling === featuresList) {
+        dropdown.classList.toggle('dropdown');
+        dropdown.nextElementSibling.classList.toggle('dropdown');
+        dropdown.nextElementSibling.nextElementSibling.classList.toggle('dropdown');
+        buttonWrapper.classList.toggle('dropdown');
+        e.target.classList.toggle('rotate');
+    }
+
+    if (e.target.nextElementSibling === companyList) {
+        dropdown.classList.toggle('dropdown2');
+        dropdown.nextElementSibling.classList.toggle('dropdown2');
+        buttonWrapper.classList.toggle('dropdown2');
+        e.target.classList.toggle('rotate');
+    }
+}
+
+hamburger.addEventListener('click', menuHandler);
+arrows.forEach(arrow => arrow.addEventListener('click', submenuHandler));
